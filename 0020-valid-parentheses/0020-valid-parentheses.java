f5ff3992c1473577f2e246d71d26c208a0abd919
Akash Chauhan <136882078+akashc-hub@users.noreply.github.com>
2024-11-12 16:00:50 +0530

    Time: 2 ms (97.16%), Space: 42 MB (12.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,68 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         while(true)
+//         {
+//             if(s.contains("()"))
+//             {
+//                 s = s.replace("()", "");
+//             }
+//             else if(s.contains("{}"))
+//             {
+//                 s = s.replace("{}", "");
+//             }
+//             else if(s.contains("[]"))
+//             {
+//                 s = s.replace("[]", "");
+//             }
+//             else
+//             {
+//                 return s.isEmpty();
+//             }
+//         }
+//     }
+// }
 class Solution {
     public boolean isValid(String s) {
-        while(true)
+        int n = s.length();
+        if(n%2!=0)
         {
-            if(s.contains("()"))
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<n; i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{')
             {
-                s = s.replace("()", "");
-            }
-            else if(s.contains("{}"))
-            {
-                s = s.replace("{}", "");
-            }
-            else if(s.contains("[]"))
-            {
-                s = s.replace("[]", "");
+                stack.push(ch);
             }
             else
             {
-                return s.isEmpty();
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+
+                char top = stack.peek();
+
+                if(ch == ')' && top == '(')
+                {
+                    stack.pop();
+                }
+                else if(ch == ']' && top == '[')
+                {
+                    stack.pop();
+                }
+                else if(ch == '}' && top == '{')
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
+        return stack.empty();
     }
 }
